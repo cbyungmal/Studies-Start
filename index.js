de170115b0db95115abbf584523e222a95bfcb3b
cbyungmal <jgaines554@noscihigh.org>
2021-02-18 18:40:46 +0000

    Loops and Functions

--- index.js
+++ index.js
@@ -68,3 +68,64 @@ means not a number, meaning undefined
 positive infinity is the rightmost  point on the number line a negative infinty is the leftmost
 // Difference between primative and complex data types
 primitative is predefined and complex is made by the programmer and is defined.
+
+// Operators
+// Assignment operators
+store a value in the object specified by the left operand
+// Assignment operators example
+a = 10; b = 20; ch = 'y'
+// Arithmetic operators
+addition, subtraction, multiplication, and division
+// Arithemetic operators example
+x+y 
+// Comparision operators
+operators that compare values and return true or false 
+// Comparison operator example 
+<,>,<=,>=,===,!== 
+// Logical Operators
+a symbol or words used to connect two expressions
+// Logical Operators examples
+(A && B) is false
+
+// With operators (+)
+indicates a outer join
+// With string methods (split, join, reverse)
+split: breaks the string into an array of strings 
+// With string methods (split, join, reverse)
+name = string.split('Jamar')
+
+// Control flow
+// If
+using the keyword if to check for a specific condition 
+// If examples
+if (/* some conditions here */) {
+  // run some code here if condition true console.log('!');
+}
+// Else If
+using the keyword else if to check for a specific condition but only if the previos statement is false
+// Else if examples
+else if (/* another condition */)
+// Else
+an alternative statement that is executed if the result of a previous test condition evaluates to false
+
+// Loops
+// Explain while and for Loops
+used to repeat a specific block of code an unknown number of times until a condition is met 
+// Explain while and for Loops example
+i <=5
+// Be able to loop any number of times forward counting up to some limit backwarding counting down to 0
+Loop over an array forwrds and backwards
+Loop over an object forwards and backwards (car = Object.car(myObject); )
+
+// Functions
+// The two phases to using functions: First we must __? Next we can execute (or two other words for executing a function?) a Function by ?  
+A function is block of statements that performs a specific task. You will need to perform the same task in the function more than once which meaans you use the same set of statements every time you want to perform a task
+// What's the difference between a function's parameters and arguments PASSED to a function?
+A parameter is the variable listed inside the parentheses in the function definition
+An argument is the vaalue that is sent to the function when it is called 
+// What's the syntax for named location?
+When one piece of code invokes or calls a function, it is done by the following syntax: variable = function_name (args, ...);
+// How do we assign a function to a varible?
+Simply assign a function to the desired variable but without () just with the name of the function 
+// Functions can optionally take inputs, and optionally return a single value. How do we specify inputs, and how do we specify outputs?
+Inputs are the signals or data recieved by the system and outputs are the signals or data sent from it
